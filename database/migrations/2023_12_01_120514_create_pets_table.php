088823bf644a158f061a51efabaf495a4d6a8a6a
Aries <ajtagle12@gmail.com>
2023-12-01 21:55:41 +0800

    trying to add pet table and breed table

---
+++ database/migrations/2023_12_01_120514_create_pets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['Cat', 'Dog']);
+            $table->unsignedBigInteger('breed_id');
+            $table->enum('sex', ['Male', 'Female']);
+            $table->enum('weight', ['5-10 lbs', '10-20 lbs', '20-50 lbs', 'More than 50 lbs']);
+            $table->enum('age', ['Less than 6 months', '6 months to 5 years', '5 to 10 years', 'More than 10 years']);
+            $table->text('about')->nullable();;
+            $table->image('photo')->nullable();;
+            $table->timestamps();
+
+            $table->foreign('breed_id')->references('id')->on('breeds');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
